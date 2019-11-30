3f2ec3d9cbecc036e2a638bbf7f0e741b680b8d5
vadim <vadikoff57@gmail.com>
2019-11-30 21:00:11 +0300

    fix: add video and fix gulpfile.js

--- repair-design-project/gulpfile.js
+++ repair-design-project/gulpfile.js
@@ -4,7 +4,7 @@ const sourcemaps = require('gulp-sourcemaps');
 const watch = require('gulp-watch');
 
 gulp.task('sass-compile' ,function(){
-return gulp.src('./scss/**/*.scss')
+return gulp.src('./scss/**/main.scss')
 .pipe(sourcemaps.init())
 .pipe(sass().on('error',sass.logError))
 .pipe(sourcemaps.write('./'))
